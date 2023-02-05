9a166ec223395b45fdb1d8f6bf69c2916692d059
Hernan Valls <hernan.r.valls@gmail.com>
2023-02-05 04:32:39 +0100

    Change css and build

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
-
+//TODO:Delete this component
 @Component({
   selector: 'shared-navbar',
   templateUrl: './navbar.component.html',
